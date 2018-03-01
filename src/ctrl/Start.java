bc92ca8b1ff1a0b3cd73a3b3035c8cb4c6dc6f2c
tapan14 <tapan14@ptl27.eecs.yorku.ca>
2018-03-01 16:13:29 -0500

    added classes as following with packages:
    	packages - Bean and DAO
    	classes - Book, PurhcaseOrder, ProductCatalog, User 
    
    added JSPX files as following: 
    	LoginPage.jspx
    	CustomerRegisterPage.jspx
    	ShoppingCart.jspx
    	
    We still gonna figure out what CSS style we want for this project

--- src/ctrl/Start.java
+++ src/ctrl/Start.java
@@ -8,8 +8,6 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-
-
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
